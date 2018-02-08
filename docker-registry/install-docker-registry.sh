7bbdf641f684b35c2e3c1926351d179ff83ca16c
Leonardo Park <ageofblue@gmail.com>
2018-02-08 13:05:09 +0900

    수정

--- docker-registry/install-docker-registry.sh
+++ docker-registry/install-docker-registry.sh
@@ -1,1 +1,2 @@
 helm install --set persistence.enabled=true,persistence.storageClass=aws-sc stable/docker-registry
+/Users/ageofblue/Developments/Workspaces/sicc/k8s/startDockerRegistry.sh
--- install_tools.sh
+++ install_tools.sh
@@ -1,6 +1,13 @@
 sudo pip install awscli
 
+# kubectl
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+or 
+
+sudo snap install kubectl --classic
 
 # kops
 curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
