de8abcbdedf313fc752d8460f5c8e7d393333f60
Leonardo Park <blue.park@kt.com>
2018-02-19 04:32:15 +0000

    ichange node size

--- create_cluster.sh
+++ create_cluster.sh
@@ -4,5 +4,5 @@ export CLUSTER_NAME=gsp.sicc.co.kr
 export VPC_ID=vpc-4fdb7927
 
 kops create cluster --name=$CLUSTER_NAME --zones=ap-northeast-2a --cloud=aws --dns-zone=gsp.sicc.co.kr \
---master-size=t2.medium --node-size=t2.small --node-count=2 --master-count=1 \
+--master-size=t2.medium --node-size=t2.medium --node-count=2 --master-count=1 \
 --vpc=$VPC_ID --topology=private --networking=calico
