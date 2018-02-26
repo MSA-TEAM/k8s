7337504f61e1b206a097eac82e9d7eaa3a08c874
leonardo.park <blue.park@kt.com>
2018-02-26 13:24:47 +0900

    add sress

--- autoscale/create.sh
+++ autoscale/create.sh
@@ -1,1 +1,4 @@
+docker build . -t auto-scale-test
+docker tag auto-scale-test docker.gsp.sicc.co.kr/auto-scale-test
+docker push docker.gsp.sicc.co.kr/auto-scale-test
 kubectl run php-apache --image=docker.gsp.sicc.co.kr/auto-scale-test --requests=cpu=200m --expose --port=80
