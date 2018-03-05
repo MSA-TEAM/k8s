b896f5f182f8c53297761ebbe1b3faf0ab8a8123
leonardo.park <blue.park@kt.com>
2018-03-05 14:02:18 +0900

    change configration

--- autoscale/create_stress.sh
+++ autoscale/create_stress.sh
@@ -1,2 +1,4 @@
 kubectl run -i --tty load-generator --image=busybox /bin/sh
+
+# kubectl exec -it php-apache-7d9d4c7485-rbxr7 /bin/sh
 # while true; do wget -q -O- http://php-apache.default.svc.cluster.local; done
--- create_cluster.sh
+++ create_cluster.sh
@@ -4,5 +4,5 @@ export CLUSTER_NAME=gsp.sicc.co.kr
 export VPC_ID=vpc-4fdb7927
 
 kops create cluster --name=$CLUSTER_NAME --zones=ap-northeast-2a --cloud=aws --dns-zone=gsp.sicc.co.kr \
---master-size=t2.large --node-size=t2.medium --node-count=2 --master-count=1 \
+--master-size=t2.large --node-size=t2.large --node-count=3 --master-count=1 \
 --vpc=$VPC_ID --topology=private --networking=calico
